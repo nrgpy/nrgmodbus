6856695d03961697765945666a5f3b9e1410e0a3
Dave <davec.vt@gmail.com>
2019-05-22 16:33:48 -0400

    added license

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+      long_description = fh.read()
 
 setup(
     name='nrgmodbus',
     version='0.1.2',
+    description='library for making modbus connections to NRG Systems devices.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nrgpy/nrgmodbus',
+    author='NRG Systems, Inc.',
+    author_email='support@nrgsystems.com',
+    licence='MIT',
+    keywords='nrg systems symphoniepro ipackaccess wind modbus'
     packages=[
         'nrgmodbus',
     ],
@@ -12,4 +22,5 @@ setup(
         'requests',
     ],
     python_requires='>=3.0',
+    zip_safe=False
 )
